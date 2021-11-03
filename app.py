39789563a7e3c3fc8a34b97a9b82b7c3bfd526c1
DuneeshaFernando <duneesha18@gmail.com>
2021-11-03 16:53:55 +0530

    Add improvements to the code

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-from flask import Flask, render_template, Response
+from flask import Flask, Response
 from random import choices
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello',methods=['POST'])
 def hello():
     random_numbers = [1, 2, 3, 4, 5, 6]
-    weights = [0.1, 0.05, 0.05, 0.2, 0.4, 0.2]
+    weights = [0.1, 0.15, 0.05, 0.1, 0.5, 0.1]
     random_number = choices(random_numbers, weights)
-    if random_number == 5:
+    if random_number[0] == 5:
         # Return a 404
-        status_code = Response(status=404)
+        status_code = Response(status=500)
         return status_code
-        # return render_template('404.html', title = '404'), 404
     else:
         # Return a 200
         status_code = Response(status=200)
         return status_code
-        # return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, threaded=True)
